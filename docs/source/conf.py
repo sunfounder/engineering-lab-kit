480b6e23c8dd45eca59b891e1ea947151fca93ce
Wulu <invincible9257@gmail.com>
2024-07-29 10:32:02 +0800

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_static_path = ['_static']
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
